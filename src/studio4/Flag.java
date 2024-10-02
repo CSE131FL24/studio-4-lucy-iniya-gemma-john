942257e49cbe42480b71e777da238c5f58bf7ba7
12067 <12067@DESKTOP-K311JHB.dhcp.wustl.edu>
2024-10-02 16:38:38 -0500

    Flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,15 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +21,22 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean bob = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double z = in.nextDouble();
+		double a = in.nextDouble();
+		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledRectangle(x, y, z, a);
+		
+		
+		
 		
 	}
 }
